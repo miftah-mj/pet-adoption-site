0e9190b0725dad03558292b2eaee750c5689a7b4
Your Name <jannat27120@gmail.com>
2024-10-11 21:33:44 +0600

    Add pet adoption site URL to CNAME file

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
+// remove active class
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName("category-btn");
+    // console.log(buttons);
+    for (let btn of buttons) {
+        btn.classList.remove("active");
+    }
+};
+
 // fetch, load and show categories on the page
 
 // create loadCategories function
@@ -7,6 +16,13 @@ const loadCategories = () => {
         .then((data) => displayCategories(data.categories))
         .catch((error) => console.log(error));
 };
+// const loadCategories = async () => {
+//     const response = await fetch(
+//         "https://openapi.programming-hero.com/api/peddy/categories"
+//     );
+//     const data = await response.json();
+//     displayCategories(data.categories);
+// };
 
 // create loadpets function
 const loadPets = () => {
@@ -16,6 +32,51 @@ const loadPets = () => {
         .catch((error) => console.log(error));
 };
 
+// create loadCategorypets function
+// const loadCategorypets = (category) => {
+//     alert(category);
+//     // fetch category pets
+//     fetch(`https://openapi.programming-hero.com/api/peddy/pet/dog`)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//             removeActiveClass();
+
+//             const activeBtn = document.getElementById(`btn-${category}`);
+//             // if (activeBtn) console.log("btn activated");
+
+//             activeBtn.classList.add("active");
+//             // console.log(activeBtn);
+//             displayPets(data.category);
+//         })
+//         .catch((error) => console.log(error));
+// };
+
+const loadCategorypets = (category) => {
+    console.log(category);
+
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+      .then((res) => res.json())
+      .then((data) => {
+        //active class remove
+        removeActiveClass();
+  
+        //id er class k active korao
+        const activeBtn = document.getElementById(`btn-${category}`);
+        activeBtn.classList.remove("rounded-md");
+        activeBtn.classList.add("active");
+        displayPets(data.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+const loadDetails = async (petId) => {
+    // console.log(petId);
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+    const response = await fetch(uri);
+    const data = await response.json();
+    displayDetails(data.petData);
+};
 
 /*
 
@@ -37,61 +98,106 @@ const loadPets = () => {
 //     pet_name: "Sunny",
 // };
 
+// displayDetails function
+const displayDetails = (pet) => {
+    console.log(pet);
+    const detailsContainer = document.getElementById("modal-content");
+
+    // add details
+    detailsContainer.innerHTML = `
+    <figure>
+            <img class="object-cover w-full h-60 mb-5"
+            src="$${pet?.image ?? "Not available"}"
+            alt="pets" />
+    </figure>
+        <div class="space-y-2">
+            <h2 class="card-title">${pet?.pet_name ?? "Not available"}</h2>
+            <p class="flex gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+            </svg>
+            Breed: ${pet?.breed ?? "Not available"}</p>
+            <p class="flex gap-2"> 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+            </svg>
+            Birth:
+            ${pet?.date_of_birth ?? "Not available"}</p>
+            <p class="flex gap-2 pets-baseline">
+            <i class="fa-solid fa-mercury"></i>
+            ${pet?.gender ?? "Not available"}</p>
+            <p class="flex gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            Price: 
+            ${pet?.price ?? "Not available"}$</p>`;
+
+    // 1st way to show modal
+    // document.getElementById("showModal-btn").click();
+
+    // 2nd way to show modal
+    document.getElementById("customModal").showModal();
+};
+
 // displayPets function
 const displayPets = (pets) => {
-    const videoContainer = document.getElementById("pets");
-    videoContainer.innerHTML = "";
+    const petContainer = document.getElementById("pets");
 
-    // if (pets.length === 0) {
-    //     // remove grid class
-    //     videoContainer.classList.remove("grid");
-    //     // add no pets found message
-    //     videoContainer.innerHTML = `
-    //     <div class="min-h-80 w-1/2 mx-auto flex flex-col justify-center items-center gap-5">
-    //     <img src="images/error.webp" />
-    //     <h2 class="text-4xl font-semibold text-center">Oops!! Sorry, There is no content here</h2>
-    //     </div>`;
-    //     return;
-    // } else {
-    //     // add grid class
-    //     videoContainer.classList.add("grid");
-    // }
+    petContainer.innerHTML = "";
+
+    if (pets.length === 0) {
+        // remove grid class
+        petContainer.classList.remove("grid");
+        // add no pets found message
+        petContainer.innerHTML = `
+        <div class="min-h-80 w-1/2 mx-auto flex flex-col justify-center pets-center gap-5">
+        <img class="w-1/2 mx-auto" src="images/error.webp" />
+        <h2 class="text-4xl font-semibold text-center">No Information Available</h2>
+        <p class="text-lg text-center">It is our mission to help you find the perfect 
+        pet for your family and to help you provide a loving home.</p>
+        </div>`;
+        return;
+    } else {
+        // add grid class
+        petContainer.classList.add("grid");
+    }
 
     // add data to the page
-    pets.forEach((item) => {
-        // console.log(item);
+    pets.forEach((pet) => {
+        // console.log(pet);
 
         // create a card
         const card = document.createElement("div");
         card.classList = "card card-compact border rounded-lg p-4";
         card.innerHTML = `
         <figure>
-            <img class="object-cover w-full h-60"
-            src="${item?.image ?? 'Not available'}"
+            <img class="object-cover w-full h-60 mb-5"
+            src="${pet?.image ?? "Not available"}"
             alt="pets" />
         </figure>
         <div class="space-y-2">
-            <h2 class="card-title">${item.pet_name}</h2>
+            <h2 class="card-title">${pet.pet_name}</h2>
             <p class="flex gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
             </svg>
-            Breed: ${item?.breed ?? 'Not available'}</p>
+            Breed: ${pet?.breed ?? "Not available"}</p>
             <p class="flex gap-2"> 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
             </svg>
             Birth:
-            ${item?.date_of_birth ?? 'Not available'}</p>
-            <p class="flex gap-2 items-baseline">
+            ${pet?.date_of_birth ?? "Not available"}</p>
+            <p class="flex gap-2 pets-baseline">
             <i class="fa-solid fa-mercury"></i>
-            ${item?.gender ?? 'Not available'}</p>
+            ${pet?.gender ?? "Not available"}</p>
             <p class="flex gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
             Price: 
-            ${item?.price ?? 'Not available'}$</p>
+            ${pet?.price ?? "Not available"}$</p>
 
             <div class="card-actions justify-around">
             <button class="btn text-primary bg-white border">
@@ -100,12 +206,13 @@ const displayPets = (pets) => {
             </svg>
             </button>
             <button class="btn text-primary bg-white border">Adopt</button>
-            <button class="btn text-primary bg-white border">Details</button>
+            <button onclick="loadDetails('${pet.petId}')"
+            class="btn text-primary bg-white border">Details</button>
             </div>
         </div>
         </div>`;
         // add button to categoryContainer
-        videoContainer.append(card);
+        petContainer.append(card);
     });
 };
 
@@ -117,30 +224,43 @@ const displayPets = (pets) => {
 
 // create displayCategories function
 const displayCategories = (categories) => {
-    const categoryContainer = document.getElementById("categories");
+    const categoryContainer = document.getElementById("category");
 
     // add data to the page
     // console.log(categories);
-    categories.forEach((pet) => {
-        console.log(pet);
+    categories.forEach((item) => {
+        // console.log(item);
+
         // create a button
         const buttonContainer = document.createElement("div");
         buttonContainer.innerHTML = `
-        <button id='btn-${pet.id}' class="category-btn rounded-full border border-primary px-10 py-2 text-lg flex gap-4 items-center justify-center">
-            <img src="${pet.category_icon}" alt="icon" class="category-icon w-8">
-            <span>${pet.category}</span>
+        <button id="btn-${item.category}" onclick="loadCategorypets('${item.category}')" class="category-btn rounded-full border border-primary px-10 py-2 text-lg flex gap-4 pets-center justify-center">
+            <img src="${item.category_icon}" alt="icon" class="category-icon w-8">
+            <span>${item.category}</span>
         </button>`;
 
         // add button to categoryContainer
         categoryContainer.append(buttonContainer);
     });
 };
+//     const categoryContainer = document.getElementById("category");
+//     // console.log(categories);
+
+//     categories.forEach((item) => {
+//         // console.log(item);
+
+//         // create button
+//         const buttonContainer = document.createElement("div");
+//         buttonContainer.innerHTML =
+//             `
+//                 <button id="btn-${item.category}" onclick="loadCategorypets('${item.category}')" class="h-8 md:h-12 lg:h-14 flex gap-2 lg:gap-3 justify-center items-center category-btn mt-4 px-4 lg:px-6 rounded-md font-semibold text-xs md:text-sm lg:text-base hover:bg-white border hover:border-primary">
+//                 <img class="w-1/3 md:w-1/4 lg:w-2/6" src='${item.category_icon}' alt="">
+//                 ${item.category}</button>
+//             `;
+//         // add button to category container
+//         categoryContainer.append(buttonContainer);
+//     });
+// };
 
 loadCategories();
 loadPets();
-
-// ${
-//     item.authors[0].verified === true
-//         ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt="verified" />`
-//         : ""
-// }
