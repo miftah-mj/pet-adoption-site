5022a381ad95e88c4a21806345497dc0ea9d74ee
Your Name <jannat27120@gmail.com>
2024-10-11 23:56:22 +0600

    Refactor sorting function and fetch pets data for sorting

--- scripts/app.js
+++ scripts/app.js
@@ -271,19 +271,53 @@ const likePet = (imageUrl) => {
     likesContainer.append(img);
 };
 
-
-function sorting (pets) {
+function sorting(pets) {
     pets.sort((a, b) => b.price - a.price);
     // console.log(pets);
     displayPets(pets);
 }
-function sort ()
-{
+function sort() {
     removeActiveClass();
-    fetch('https://openapi.programming-hero.com/api/peddy/pets')
-    .then((response) => response.json())
-    .then((data => sorting(data.pets)))
+    fetch("https://openapi.programming-hero.com/api/peddy/pets")
+        .then((response) => response.json())
+        .then((data) => sorting(data.pets));
 }
+
+const countdownElement = document.getElementById("countdown");
+const customAdopt = document.getElementById("customAdopt");
+let countdownInterval; // Declare the interval globally
+
+function startCountdown() {
+    let counter = 3;
+
+    // Start the countdown and update the global countdownInterval variable
+    countdownInterval = setInterval(() => {
+        if (counter > 1) {
+            counter--;
+            countdownElement.innerText = counter;
+        } else {
+            customAdopt.close(); // Close the modal
+            clearInterval(countdownInterval);
+        }
+    }, 1000); // Update every second
+}
+
+const displayAdopt = (button, petId) => {
+    button.innerText = "Adopted";
+    button.disabled = true;
+
+    if (button.disabled) {
+        button.style.border = "none";
+    }
+    // console.log(petId);
+
+    // way 1
+    // document.getElementById("showAdoptData").click();
+    // way 2
+    customAdopt.showModal();
+    startCountdown();
+    countdownElement.innerText = 3;
+};
 
 loadCategories();
 loadPets();
