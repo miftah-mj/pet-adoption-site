47a8e50d4b0afb0ff79ff97b66ab7ebe5784dfbf
Your Name <jannat27120@gmail.com>
2024-10-11 23:07:20 +0600

    added likes functionality

--- scripts/app.js
+++ scripts/app.js
@@ -192,7 +192,7 @@ const displayPets = (pets) => {
             ${pet?.price ?? "Not available"}$</p>
 
             <div class="card-actions justify-around">
-            <button class="btn text-primary bg-white border">
+            <button class="btn text-primary bg-white border" onclick="likePet('${pet.image}')">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
             </svg>
@@ -235,24 +235,22 @@ const displayCategories = (categories) => {
         categoryContainer.append(buttonContainer);
     });
 };
-//     const categoryContainer = document.getElementById("category");
-//     // console.log(categories);
 
-//     categories.forEach((item) => {
-//         // console.log(item);
 
-//         // create button
-//         const buttonContainer = document.createElement("div");
-//         buttonContainer.innerHTML =
-//             `
-//                 <button id="btn-${item.category}" onclick="loadCategorypets('${item.category}')" class="h-8 md:h-12 lg:h-14 flex gap-2 lg:gap-3 justify-center items-center category-btn mt-4 px-4 lg:px-6 rounded-md font-semibold text-xs md:text-sm lg:text-base hover:bg-white border hover:border-primary">
-//                 <img class="w-1/3 md:w-1/4 lg:w-2/6" src='${item.category_icon}' alt="">
-//                 ${item.category}</button>
-//             `;
-//         // add button to category container
-//         categoryContainer.append(buttonContainer);
-//     });
-// };
+
+// likes
+const likePet = (imageUrl) => {
+    const likesContainer = document.getElementById("likes");
+
+    // Create an image element
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.alt = "Liked Pet";
+    img.classList.add("liked-pet-image");
+
+    // Add the image to the likes container
+    likesContainer.append(img);
+};
 
 loadCategories();
 loadPets();
